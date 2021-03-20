bdbb53ddff3353875daebac59c9d4ccd94542b76
Vedshri-18 <vedashridhoke@gmail.com>
2021-03-20 23:15:42 +0530

    task: recreated addition method

--- Calculator/CalculatorTests.py
+++ Calculator/CalculatorTests.py
@@ -3,6 +3,8 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+
+
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3
 
-ADD src /src
+ADD Calculator /src
 
 CMD [ "python" , "./src/CalculatorTests.py" ]
